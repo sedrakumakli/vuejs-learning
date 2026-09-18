9d3e167ca4cfbb4baecd0a813291dec7e39c855c
Sedra Komakli <asmahanasmahan045@gmail.com>
2026-09-18 15:45:38 +0300

    from 8 to 10

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
 const {createApp} = Vue; 
 
 createApp({
-   data () {
-    return{
-   //   name : "Ahmad",
-   //   age : 23,
-     link : "https://google.com",
-     num : 0,
-    };
+   // from 1 to 7:
+   // data () {
+   //  return{
+   // name : "Ahmad",
+   //  age : 23,
+   //   link : "https://google.com",
+   //   num : 0,
+   //  };
+   // },
+   // methods:{
+   //    sayHi( name , fram){
+   //         return "Hello" + " " + name + " , " + " my frameWork is " + fram; 
+   //    },
+   //    write(){
+   //       console.log("new project");
+   //    },
+   //    increase(){
+   //       this.num++;
+   //    },
+   //    testme(){
+   //       console.log("once");
+   //    }
+   // },
+   // from 8 to :
+   data(){
+      return{
+         // 8 and 9 :
+      //   age : 23,
+      //   isActive : false,
+      //   newUser :{
+      //   name : "",
+      //   email: "",
+      //   password:"",
+      //   },
+      //   myArr : [
+      //    {food : "banana" , price : 300}, 
+      //    {food : "apple" , price : 400},
+      // ],
+      numberOne: 0,
+      numberTwo : 0,
+      };
    },
    methods:{
-      sayHi( name , fram){
-           return "Hello" + " " + name + " , " + " my frameWork is " + fram; 
+      //8 and 9:
+      // sendInfo(){
+      //   const userInfo = {
+      //    name : this.newUser.name,
+      //    email : this.newUser.email,
+      //    password : this.newUser.password,
+      //   }
+      //   console.log(userInfo);
+      // }
+   },
+   computed:{
+   // 10 :
+      numberOneChange(){
+         return this.numberOne * 2;
       },
-      write(){
-         console.log("new project");
-      },
-      increase(){
-         this.num++;
-      },
-      testme(){
-         console.log("once");
+      numberTwoChange(){
+         return this.numberTwo +10;
       }
    },
    
